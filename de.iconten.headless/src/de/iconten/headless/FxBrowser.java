e1fb35fcd350268ab14d8a65a5cdce42ff8e8aee
Michael Maenz <mmaenz@yahoo.de>
2018-09-03 11:14:25 +0200

    Works

--- de.iconten.headless/src/de/iconten/headless/FxBrowser.java
+++ de.iconten.headless/src/de/iconten/headless/FxBrowser.java
@@ -4,13 +4,25 @@ import java.util.concurrent.CountDownLatch;
 
 import com.sun.glass.ui.monocle.MonocleLauncher;
 
-import javafx.beans.value.ChangeListener;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
 
 public class FxBrowser {
+	public static class FxData {
+		private static String content = "";
+
+		public static void set(String s) {
+			content = s;
+		}
+
+		public static String get() {
+			return content;
+		}
+	}
+
 	private static FxBrowser instance;
 
 	public static FxBrowser getFxBrowser() {
@@ -26,7 +38,7 @@ public class FxBrowser {
 		new Thread(() -> {
 			try {
 				new MonocleLauncher().launch(FxApplication.class,
-						new String[] { Integer.toString(1024), Integer.toString(768), Boolean.toString(false) });
+						new String[] { Integer.toString(1024), Integer.toString(768), Boolean.toString(true) });
 			} catch (final Throwable t) {
 				System.out.println(t.getMessage());
 			}
@@ -37,40 +49,46 @@ public class FxBrowser {
 	public static void main(String... args) throws Exception {
 		final FxBrowser browser = FxBrowser.getFxBrowser();
 		browser.runFxBrowser();
+		Platform.exit();
 	}
 
 	public String getTitleFromPage(String url) throws InterruptedException {
 		final CountDownLatch latch = new CountDownLatch(1);
-		final String title = "";
+		final String title;
 		final Task<String> google = new Task<String>() {
 			@Override
-			public String call() {
-				final WebView view = FxApplication.getView();
-				view.getEngine().getLoadWorker().stateProperty().addListener((ChangeListener<State>) (observable, oldValue, newValue) -> {
-					if (newValue != Worker.State.SUCCEEDED) {
-						return;
-					}
-					set(view.getEngine().getTitle());
-					return;
-				});
-				view.getEngine().load(url);
+			protected String call() throws Exception {
+				try {
+					final WebEngine engine = FxApplication.getView().getEngine();
+					engine.load(url);
+
+					engine.getLoadWorker().stateProperty()
+							.addListener((ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) -> {
+								if (newValue != Worker.State.SUCCEEDED) {
+									return;
+								}
+
+								FxData.set(engine.getTitle());
+								latch.countDown();
+							});
+					return null;
+				} catch (final Exception ex) {
+					System.out.println(ex.getMessage());
+				}
 				return null;
 			}
 		};
 
-		google.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, t -> {
-			final String result = google.getValue();
-		});
-
-		latch.wait();
-		return title;
+		Platform.runLater(google);
+		latch.await();
+		return FxData.get();
 	}
 
 	public void runFxBrowser() throws Exception {
 		while (!FxApplication.isInitDone()) {
 			Thread.sleep(100);
 		}
-		String test = getTitleFromPage("https://www.google.de");
-		test = test;
+		final String test = getTitleFromPage("https://www.google.de");
+		System.out.println(test);
 	}
 }
